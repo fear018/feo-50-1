3597186fe2be3835605fc67866c0064cb87c74a8
Rostislav Radionov <rostislav.radionov018@gmail.com>
2023-07-05 20:49:39 +0200

    added: updates

--- arrays.js
+++ arrays.js
@@ -4,7 +4,20 @@
 // let arr = [item1, item2...];
 
 // // new Array (очень редко)
-// let arr = new Array(item1, item2...);
+// const arr = [1, 2, 3, 4, 5, "232", false, { name: "John" }];
+// // for (let i = 0; i < arr.length; i++) {
+// //   console.log(arr[i]);
+// // }
+
+// for (let item of arr) {
+//   console.log(item);
+// }
+// console.log(arr);
+// console.log(arr[0]);
+// console.log(arr[6]);
+// console.log(arr.length);
+// const arrLength = arr.length;
+// console.log(arr[arrLength - 1]);
 
 // Мы можем получить элемент по его индексу, например arr[0].
 
@@ -14,6 +27,11 @@
 
 // Методы массивов
 // Полный список есть в справочнике MDN.
+// const arr = [1, 2, 3];
+// // arr.push(5);
+// // console.log(arr);
+// arr.unshift("qweqwe");
+// console.log(arr);
 // push(...items)добавляет items в конец массива.
 // pop() удаляет элемент в конце массива и возвращает его.
 // shift() удаляет элемент в начале массива и возвращает его.
@@ -21,86 +39,158 @@
 
 // let arr = ["I", "go", "home"];
 // delete arr[1]; // удалить "go"
-// alert(arr[1]); // undefined
+// console.log(arr); // undefined
+// console.log(arr.length); // undefined
 // // теперь arr = ["I",  , "home"];
 // alert(arr.length); // 3
 
 // Для добавления/удаления элементов:
 // splice(pos, deleteCount, ...items) – начиная с индекса pos удаляет deleteCount элементов и вставляет items.
 // let arr = ["Я", "изучаю", "JavaScript"];
-// arr.splice(1, 1); // начиная с индекса 1, удалить 1 элемент
+// arr.splice(1, 0, "LOVE"); // начиная с индекса 1, удалить 1 элемент
+// console.log(arr);
 
 // удалить 3 первых элемента и заменить их другими
 
 // slice(start, end) – создаёт новый массив, копируя в него элементы с индекса start до end (не включая end).
 // let arr = ["t", "e", "s", "t"];
-// alert( arr.slice(1, 3) ); // e,s (копирует с 1 до 3)
+// console.log(arr.slice(1, 3)); // e,s (копирует с 1 до 3)
 
 // concat(...items) – возвращает новый массив: копирует все члены текущего массива и добавляет к нему items. Если какой-то из items является массивом, тогда берутся его элементы.
 // let arr = [1, 2];
-// // создать массив из: arr и [3,4]
-// alert( arr.concat([3, 4]) ); // 1,2,3,4
-// // создать массив из: arr и [3,4] и [5,6]
-// alert( arr.concat([3, 4], [5, 6]) ); // 1,2,3,4,5,6
-// // создать массив из: arr и [3,4], потом добавить значения 5 и 6
-// alert( arr.concat([3, 4], 5, 6) ); // 1,2,3,4,5,6
+// // // создать массив из: arr и [3,4]
+// console.log(arr.concat([3, 4])); // 1,2,3,4
+// // // создать массив из: arr и [3,4] и [5,6]
+// console.log(arr.concat([3, 4], [5, 6])); // 1,2,3,4,5,6
+// // // создать массив из: arr и [3,4], потом добавить значения 5 и 6
+// console.log(arr.concat([3, 4], 5, 6)); // 1,2,3,4,5,6
 
 // Для поиска среди элементов:
 // indexOf/lastIndexOf(item, pos) – ищет item, начиная с позиции pos, и возвращает его индекс или -1, если ничего не найдено.
-// let arr = [1, 0, false];
-// alert( arr.indexOf(0) ); // 1
+// let arr = [1, 2, 3, 4, 5, false];
+// console.log(arr.indexOf(false)); // 1
 
 // let fruits = ['Яблоко', 'Апельсин', 'Яблоко']
 // alert( fruits.indexOf('Яблоко') ); // 0 (первый 'Яблоко')
 // alert( fruits.lastIndexOf('Яблоко') ); // 2 (последний 'Яблоко')
 
 // includes(value) – возвращает true, если в массиве имеется элемент value, в противном случае false.
-// fruits.includes('Яблоко'); // true
+// let fruits = ["Яблоко", "Апельсин", "Яблоко"];
+// console.log(fruits.includes("Апельсин")); // true
 
 // find/filter(func) – фильтрует элементы через функцию и отдаёт первое/все значения, при прохождении которых через функцию возвращается true.
-// let result = arr.find(function(item, index, array) {
-//   // если true - возвращается текущий элемент и перебор прерывается
-//   // если все итерации оказались ложными, возвращается undefined
-// });
 // let users = [
-//   {id: 1, name: "Вася"},
-//   {id: 2, name: "Петя"},
-//   {id: 3, name: "Маша"}
+//   { id: 1, name: "Вася" },
+//   { id: 2, name: "Петя" },
+//   { id: 3, name: "Маша" },
 // ];
 
-// let user = users.find(item => item.id == 1);
+// let result = users.find(function (item, index, array) {
+//   if (item.id === 1) {
+//     return true;
+//   }
+// });
+
+// let user = users.find((item) => item.id === 2);
+// console.log(user);
 
 // alert(user.name);
 
 // let users = [
-//   {id: 1, name: "Вася"},
-//   {id: 2, name: "Петя"},
-//   {id: 3, name: "Маша"}
+//   { id: 1, name: "Вася" },
+//   { id: 2, name: "Петя" },
+//   { id: 3, name: "Маша" },
 // ];
 
 // // возвращает массив, состоящий из двух первых пользователей
-// let someUsers = users.filter(item => item.id < 3);
+// let someUsers = users.filter((item) => item.id < 3);
+// console.log(someUsers); // 2
 
 // alert(someUsers.length); // 2
 // findIndex похож на find, но возвращает индекс вместо значения.
+// let userIndex = users.findIndex((item) => item.id > 2);
+// console.log(userIndex);
 
 // Для перебора элементов:
 // forEach(func) – вызывает func для каждого элемента. Ничего не возвращает.
 // arr.forEach(function(item, index, array) {
 //   // ... делать что-то с item
 // });
-// ["Бильбо", "Гэндальф", "Назгул"].forEach(alert);
+// const names = [];
+// const usersNames = ["Бильбо", "Гэндальф", "Назгул"];
+// usersNames.forEach((item) => {
+//   if (item.length === 6) {
+//     names.push(item);
+//   }
+// });
+// console.log(names);
 
 // Для преобразования массива:
 // map(func) – создаёт новый массив из результатов вызова func для каждого элемента.
+// let arr = ["Яблоко", "Апельсин", "Груша"];
+// let arrLength = arr.map((item) => item.toUpperCase());
+// console.log(arr); // 6,8,6
+// console.log(arrLength); // 6,8,6
 // let lengths = ["Бильбо", "Гэндальф", "Назгул"].map(item => item.length);
 // alert(lengths); // 6,8,6
 
+// reverse() – «на месте» меняет порядок следования элементов на противоположный и возвращает изменённый массив.
+// let arr = [1, 2, 3, 4, 5];
+// arr.reverse();
+// console.log(arr); // 5,4,3,2,1
+
+// split/join – преобразует строку в массив и обратно.
+// let names = "Вася, Петя, Маша";
+// let arr = names.split("");
+// console.log(arr); // Вася,Петя,Маша
+// join
+// let arr2 = ["Вася", "Петя", "Маша"];
+// let str = arr2.join(", ");
+// console.log(str); // Вася,Петя,Маша
+
+// reduce/reduceRight(func, initial) – вычисляет одно значение на основе всего массива, вызывая func для каждого элемента и передавая промежуточный результат между вызовами.
+// let value = arr.reduce(
+//   function (accumulator, item, index, array) {
+//     // ...
+//   },
+//   [initial]
+// );
+
+// accumulator – результат предыдущего вызова этой функции, равен initial при первом вызове (если передан initial),
+// item – очередной элемент массива,
+// index – его позиция,
+// array – сам массив.
+
+// let arr = [1, 2, 3, 4, 5];
+
+// let result = arr.reduce(function (accumulator, item) {
+//   console.log(accumulator, item);
+//   return accumulator + item;
+// }, 0);
+
+// console.log(result); // 15
+
+// Дополнительно:
+// Array.isArray(arr) проверяет, является ли arr массивом.
+// console.log(Array.isArray("[1, 2, 3]")); // true
+
+// Метод some() проверяет, удовлетворяет ли какой-либо элемент массива условию, заданному в передаваемой функции.
+// const array = [1, 2, 3, 4, 5];
+// const array2 = [2, 4];
+
+// console.log(array.some((item) => item % 2 === 0));
+// console.log(array2.every((item) => item % 2 === 0));
+// // Expected output: true
+
+// Метод every() проверяет, удовлетворяют ли все элементы массива условию, заданному в передаваемой функции.
+
 // sort(func) – сортирует массив «на месте», а потом возвращает его.
-// let arr = [ 1, 2, 15 ];
+// let arr = [1, 2, 15, 1231231];
 
 // // метод сортирует содержимое arr
-// arr.sort();
+// arr.sort((a, b) => a - b);
+// arr.sort((a, b) => b - a);
+// console.log(arr); // 1, 15, 2
 // лексикографический порядок
 // alert( arr );  // 1, 15, 2
 // var myarray=[25, 8, 7, 41]
@@ -112,44 +202,3 @@
 // myarray.sort(function(a,b){
 //   return b — a
 // }) //Массив становится [41, 25, 8, 7]
-
-// reverse() – «на месте» меняет порядок следования элементов на противоположный и возвращает изменённый массив.
-// let arr = [1, 2, 3, 4, 5];
-// arr.reverse();
-
-// alert( arr ); // 5,4,3,2,1
-
-// split/join – преобразует строку в массив и обратно.
-// let names = 'Вася, Петя, Маша';
-// let arr = names.split(', ');
-// join
-
-// reduce/reduceRight(func, initial) – вычисляет одно значение на основе всего массива, вызывая func для каждого элемента и передавая промежуточный результат между вызовами.
-// let value = arr.reduce(function(accumulator, item, index, array) {
-//   // ...
-// }, [initial]);
-
-// accumulator – результат предыдущего вызова этой функции, равен initial при первом вызове (если передан initial),
-// item – очередной элемент массива,
-// index – его позиция,
-// array – сам массив.
-
-// let arr = [1, 2, 3, 4, 5];
-
-// let result = arr.reduce((sum, current) => sum + current, 0);
-
-// alert(result); // 15
-
-// Дополнительно:
-// Array.isArray(arr) проверяет, является ли arr массивом.
-
-// Метод some() проверяет, удовлетворяет ли какой-либо элемент массива условию, заданному в передаваемой функции.
-// const array = [1, 2, 3, 4, 5];
-
-// // Checks whether an element is even
-// const even = (element) => element % 2 === 0;
-
-// console.log(array.some(even));
-// // Expected output: true
-
-// Метод every() проверяет, удовлетворяют ли все элементы массива условию, заданному в передаваемой функции.
--- objects.js
+++ objects.js
@@ -5,7 +5,11 @@
 // Значения могут быть любого типа.
 
 // let user = new Object(); // синтаксис "конструктор объекта"
-// let user = {};  // синтаксис "литерал объекта"
+// let user1 = {}; // синтаксис "литерал объекта"
+
+// const obj = {
+//   name: "John",
+// };
 
 // Чтобы получить доступ к свойству, мы можем использовать:
 // Запись через точку: obj.property.
@@ -21,10 +25,19 @@
 // Для удаления свойства мы можем использовать оператор delete:
 // delete user.age;
 
+// const obj = {
+//   name: "John",
+// };
+
+// obj.age = 234;
+
+// delete obj.name;
+// console.log(obj);
+
 // let user = { name: "John", age: 30 };
 
-// alert( "age" in user ); // true, user.age существует
-// alert( "blabla" in user ); // false, user.blabla не существует
+// console.log("age" in user); // true, user.age существует
+// console.log("blabla" in user); // false, user.blabla не существует
 
 // Для перебора всех свойств объекта используется цикл for..in.
 // for (key in object) {
@@ -34,14 +47,16 @@
 // let user = {
 //   name: "John",
 //   age: 30,
-//   isAdmin: true
+//   isAdmin: true,
 // };
+
+// console.log(user["name"]);
 
 // for (let key in user) {
 //   // ключи
-//   alert( key );  // name, age, isAdmin
+//   console.log("key =>", key); // name, age, isAdmin
 //   // значения ключей
-//   alert( user[key] ); // John, 30, true
+//   console.log("user[key] =>", user[key]); // John, 30, true
 // }
 
 // Упорядочение свойств объекта
@@ -50,80 +65,114 @@
 // остальные располагаются в порядке создания.
 
 // let codes = {
-//   "49": "Германия",
-//   "41": "Швейцария",
-//   "44": "Великобритания",
-//   // ..,
-//   "1": "США"
+//   49: "Германия",
+//   41: "Швейцария",
+//   44: "Великобритания",
+//   1: "США",
 // };
 
 // for (let code in codes) {
-//   alert(code); // 1, 41, 44, 49
+//   console.log(code); // 1, 41, 44, 49
 // }
 
 // Копирование объектов и ссылки
 // Переменная, которой присвоен объект, хранит не сам объект,
 // а его «адрес в памяти» – другими словами, «ссылку» на него.
 
-// let user = { name: 'John' };
+// let user = { name: "John" };
 // let admin = user;
 
-// admin.name = 'Pete'; // изменено по ссылке из переменной "admin"
-// alert(user.name);
+// admin.name = "Pete"; // изменено по ссылке из переменной "admin"
+// console.log("user", user.name);
+// console.log("admin", admin.name);
 
 // clone = {}; // новый пустой объект
 // let user = {
 //   name: "John",
-//   age: 30
+//   age: 30,
 // };
 
 // let clone = {}; // новый пустой объект
 
-// // давайте скопируем все свойства user в него
+// // // давайте скопируем все свойства user в него
 // for (let key in user) {
 //   clone[key] = user[key];
 // }
 
-// // теперь clone это полностью независимый объект с тем же содержимым
+// // // теперь clone это полностью независимый объект с тем же содержимым
 // clone.name = "Pete"; // изменим в нём данные
+// console.log(user);
+// console.log(clone);
 
 // alert( user.name ); // все ещё John в первоначальном объекте
 
 // Object.assign
 // const target = { a: 1, b: 2 };
 // const source = { c: 4, d: 5 };
+// const obj = {};
+// Object.assign(obj, source);
+// obj.c = 26;
 
-// Object.assign(target, source);
-// console.log(target);
+// console.log(obj);
+// console.log(source);
 
 // Методы объекта, "this"
 // пользователь может совершать действия
-// let user = {
-//   name: "John",
-//   age: 30
-// };
+let user = {
+  name: "John",
+  age: 30,
+  sayHi: function (gender) {
+    // this - это "текущий объект"
+    console.log(this);
+    console.log(`Hello my name is - ${this.name}`);
+    console.log(`Hello my gender is - ${gender}`);
+  },
+};
 
-// user.sayHi = function() {
-//   alert("Привет!");
-// };
-
-// user.sayHi();
+// user.sayHi("FEMALE"); // Hello my name is - John
 
 // Опциональная цепочка '?.'
 // Проблема «несуществующего свойства»
+// let user = {
+//   address: {
+//     street: "Главная 1",
+//   },
+// };
+
 // let user = {}; // пользователь без свойства "address"
-// alert(user.address.street); // Ошибка!
-// alert(user?.address.street); // Ошибка!
+// // console.log(user.address.street); // Ошибка!
+// console.log(user?.address?.street); // Ошибка!
 
 // Деструктурирующее присваивание
 // let options = {
 //   title: "Menu",
 //   width: 100,
-//   height: 200
+//   height: 200,
 // };
 
-// let {title, width, height} = options;
+// const { title } = options;
+// const title = options.title;
+// const width = options.width;
+// const height = options.height;
+// console.log(title); // Menu
+// console.log(options.title); // Menu
+// console.log(options.width); // 100
+// console.log(options.height); // 200
 
-// alert(title);  // Menu
-// alert(width);  // 100
-// alert(height); // 200
+// const getData = () => {
+//   const name = prompt("Введите имя");
+//   const surname = prompt("Введите фамилию");
+//   const experience = +prompt("Введите опыт работы в годах");
+
+//   return {
+//     name: name,
+//     surname: surname,
+//     experience: experience,
+//   };
+
+//   // return { name, surname, experience };
+// };
+
+// // data.name, data.surname, data.experience
+// const { name, surname, experience } = getData();
+// console.log(name, surname, experience);
