222e5110d3471bf15d6420c00859473e1fb009c9
Rostislav Radionov <rostislav.radionov018@gmail.com>
2023-07-10 20:54:08 +0200

    added: functions & objects & classes & prototype

---
+++ func-high.js
@@ -0,0 +1,20 @@
+// остаточные параметры
+// function sumAll(...args) {
+//   // args — имя массива
+//   console.log(args); // 1,2,3,4,5
+// }
+
+// sumAll(1, 2, 3, 4, 5);
+
+// параметры расширения
+
+// рекурсия
+// function pow(x, n) {
+//   if (n === 1) {
+//     return x;
+//   } else {
+//     return x * pow(x, n - 1);
+//   }
+// }
+
+// alert(pow(2, 3)); // 8
--- this-new.js
+++ this-new.js
@@ -1,9 +1,182 @@
-// THIS
-// Функции, которые находятся в свойствах объекта, называются «методами».
-// Методы позволяют объектам «действовать»: object.doSomething().
-// Методы могут ссылаться на объект через this.
+// "use strict";
 
+// console.log(this);
+// let user = {
+//   name: "John",
+//   age: 30,
+// };
+
+// console.log(Object.keys(user));
+// console.log(Object.values(user));
+// console.log(Object.entries(user));
+
+//
+// let arr = ["John", "Doe"];
+// // деструктурирующее присваивание
+// let [firstName, surname] = arr;
+// console.log(firstName, surname);
+
+// alert(firstName); // John
+// alert(surname);  // Doe
+
+// второй элемент не нужен
+// let [firstName, , title] = ["Julius", "Caesar", "Consul", "of the Roman Republic"];
+// alert( title ); // Consul
+
+//
+// Остаточные параметры «…»
+// let [name1, name2, ...rest] = ["Julius", "Caesar", "Consul", "Roman Republic"];
+// rest это массив элементов, начиная с 3-го
+// console.log(name1);
+// console.log(name2);
+// console.log(rest);
+
+//
+// Конструктор, оператор "new" для создания однотипных объектов
+// function user(name, surname, email, phone) {
+//   this.name = name;
+//   this.surname = surname;
+//   this.email = email;
+//   this.phone = phone;
+
+//   this.getFullName = function () {
+//     console.log(`Name: ${this.name}, Surname: ${this.surname}`);
+//     return {
+//       name: this.name,
+//       surname: this.surname,
+//     };
+//   };
+// }
+
+// let user1 = new user("Jack", "Sparrow", "email@sd.com", "123456789");
+// let user2 = new user("John", "Doe", "em222ail@sd.com", "456789");
+
+// console.log(user1);
+// console.log(user2);
+
+// let user = new User("Jack");
+// alert(user.name); // Jack
+// alert(user.isAdmin); // false
+
+// Когда функция вызывается как new User(...), происходит следующее:
+// Создаётся новый пустой объект, и он присваивается this.
+// Выполняется тело функции. Обычно оно модифицирует this, добавляя туда новые свойства.
+// Возвращается значение this.
+// Другими словами, new User(...) делает что-то вроде:
+
+// function User(name) {
+//   // this = {};  (неявно)
+
+//   // добавляет свойства к this
+//   this.name = name;
+//   this.isAdmin = false;
+
+//   // return this;  (неявно)
+// }
+// Таким образом, let user = new User("Jack") возвращает тот же результат, что и:
+
+// let user = {
+//   name: "Jack",
+//   isAdmin: false
+// };
+
+// методы конструктора создание
+// function User(name) {
+//   this.name = name;
+
+//   this.sayHi = function() {
+//     alert( "Меня зовут: " + this.name );
+//   };
+// }
+
+// let john = new User("John");
+// john.sayHi(); // Меня зовут: John
+
+//
+
+// THIS
+// call / apply / bind
+// Метод call() вызывает метод (функцию) с указанным значением this
+// и индивидуально предоставленными аргументами.
+
+// const user = {
+//   name: "John USER",
+//   age: 303,
+// };
+
+// const person = {
+//   name: "John",
+//   age: 30,
+//   getData: function (pow = 1) {
+//     console.log(`Name: ${this.name}, Age: ${this.age}`);
+//     return { name: this.name, age: this.age, pow: this.age ** pow };
+//   },
+// };
+// console.log(person.getData());
+// const person1 = person.getData.call(user, 3);
+// console.log(person1);
+
+// Метод apply() вызывает функцию с указанным значением this
+// и аргументами, предоставленными в виде массива
+
+// const user = {
+//   name: "John USER",
+//   age: 303,
+// };
+
+// const person = {
+//   name: "John",
+//   age: 30,
+//   getData: function (pow = 1) {
+//     console.log(`Name: ${this.name}, Age: ${this.age}`);
+//     return { name: this.name, age: this.age, pow: this.age ** pow };
+//   },
+// };
+// console.log(person.getData());
+// const person1 = person.getData.apply(user, [2]);
+// console.log(person1);
+
+// Метод bind() создаёт новую функцию, которая при вызове устанавливает
+// в качестве контекста выполнения this предоставленное значение.
+// В метод также передаётся набор аргументов,
+// которые будут установлены перед переданными в привязанную
+// функцию аргументами при её вызове.
+
+// const user = {
+//   name: "John USER",
+//   age: 303,
+// };
+
+// const person = {
+//   name: "John",
+//   age: 30,
+//   getData: function (pow = 1) {
+//     console.log(`Name: ${this.name}, Age: ${this.age}`);
+//     return { name: this.name, age: this.age, pow: this.age ** pow };
+//   },
+// };
+// console.log(person.getData());
+// const personBind = person.getData.bind(user);
+// console.log("func", personBind);
+// console.log(personBind());
+
+// function showThis() {
+//   const name = "Jack";
+
+//   const user = {
+//     name: this,
+//   };
+
+//   console.log(user);
+// }
+
+// showThis();
 // Определение this:
+// 1) Глобальный контекст
+// 2) Контекст функции конструктора
+// 3) Контекст вызова call / apply / bind
+// 4) Контекст метода объекта
+// 5) Контекст стрелочной функции
 
 // Мы находимся внутри функции?
 // Да: смотрим следующий пункт.
@@ -37,30 +210,216 @@
 // Да: this равняется undefined.
 // Нет: this равен глобальному объекту.
 
-// NEW
-// конструкторы для создания множества похожих объектов.
-// Функции-конструкторы технически являются обычными функциями. Но есть два соглашения:
+// =====>
+// Прототипное наследование
+// цепочка прототипов
 
-// Имя функции-конструктора должно начинаться с большой буквы.
-// Функция-конструктор должна выполняться только с помощью оператора "new".
+// Свойство [[Prototype]] является внутренним и скрытым,
+// но есть способ задать его.
+// использование __proto__,
 
-// Создаётся новый пустой объект, и он присваивается this.
-// Выполняется тело функции. Обычно оно модифицирует this, добавляя туда новые свойства.
-// Возвращается значение this.
-// Другими словами, new User(...) делает что-то вроде:
+// let animal = {
+//   eats: true,
+// };
 
-// function User(name) {
-//   // this = {};  (неявно)
+// let rabbit = {
+//   jumps: true,
+// };
 
-//   // добавляет свойства к this
+// rabbit.__proto__ = animal;
+
+// console.log(rabbit);
+// console.log(rabbit.eats);
+
+//
+
+// let animal = {
+//   eats: true,
+//   walk() {
+//     alert("Animal walk");
+//   },
+// };
+
+// let rabbit = {
+//   jumps: true,
+//   __proto__: animal,
+// };
+
+// // walk взят из прототипа
+// rabbit.walk(); // Animal walk
+
+//
+
+// let animal = {
+//   eats: true,
+//   walk() {
+//     alert("Animal walk");
+//   },
+// };
+
+// let rabbit = {
+//   jumps: true,
+//   __proto__: animal,
+// };
+
+// let longEar = {
+//   earLength: 10,
+//   __proto__: rabbit,
+// };
+
+// walk взят из цепочки прототипов
+// longEar.walk(); // Animal walk
+// alert(longEar.jumps); // true (из rabbit)
+// console.log(longEar);
+
+// 1 - [[Prototype]];
+// 2 - __proto__;
+
+// 3 - prototype;
+// ===========>
+// F.prototype
+// Как мы помним, новые объекты могут быть созданы
+// с помощью функции-конструктора new F().
+
+// Если в F.prototype содержится объект, оператор new устанавливает его
+// в качестве [[Prototype]] для нового объекта.
+
+// let animal = {
+//   eats: true,
+// };
+
+// function Rabbit(name) {
 //   this.name = name;
-//   this.isAdmin = false;
+// }
 
-//   this.sayHi = function () {
-//     console.log("Меня зовут: " + this.name);
+// Rabbit.prototype = animal;
+// console.log(Rabbit.prototype);
+
+// let rabbit = new Rabbit("White Rabbit"); //  rabbit.__proto__ == animal
+
+// console.log(rabbit); // true
+
+// =====
+// Заимствование у прототипа
+// let obj = {
+//   0: "Hello",
+//   1: "world!",
+//   length: 2,
+// };
+
+// console.log(obj);
+// console.log(Array);
+// console.log(Array.prototype);
+// console.log(Array.prototype.join);
+
+// obj.join = Array.prototype.join;
+
+// console.log(obj.join(", ")); // Hello,world!
+
+// ====================> Class
+// class User {
+//   constructor(name) {
+//     this.name = name;
+//   }
+
+//   changeName(name) {
+//     this.name = name;
+//   }
+// }
+
+// function User1(name) {
+//   this.name = name;
+
+//   this.changeName = function (name) {
+//     this.name = name;
 //   };
 // }
 
-// const user = new User("Вася");
-// console.log(user); // Вася
-// user.sayHi();
+// // доказательство: User - это функция
+// alert(typeof User); // function
+
+// Вот что на самом деле делает конструкция class User {...}:
+// Создаёт функцию с именем User, которая становится результатом объявления класса.
+// Код функции берётся из метода constructor (она будет пустой, если такого метода нет).
+// Сохраняет все методы, такие как sayHi, в User.prototype.
+
+// наследование
+// class Animal {
+//   constructor(name) {
+//     this.speed = 0;
+//     this.name = name;
+//   }
+
+//   run(speed) {
+//     this.speed = speed;
+//     console.log(`${this.name} бежит со скоростью ${this.speed}.`);
+//   }
+
+//   stop() {
+//     this.speed = 0;
+//     console.log(`${this.name} стоит неподвижно.`);
+//   }
+// }
+
+// let animal1 = new Animal("Мой питомец");
+// class Rabbit extends Animal {
+//   hide() {
+//     console.log(`${this.name} прячется!`);
+//   }
+// }
+
+// let rabbit = new Rabbit("Белый кролик");
+
+// rabbit.run(5); // Белый кролик бежит со скоростью 5.
+// rabbit.hide(); // Белый кролик прячется!
+
+// class Animal {
+//   constructor(name) {
+//     this.speed = 0;
+//     this.name = name;
+//   }
+
+//   run(speed) {
+//     this.speed = speed;
+//     console.log(`${this.name} бежит со скоростью ${this.speed}.`);
+//   }
+
+//   stop() {
+//     this.speed = 0;
+//     console.log(`${this.name} стоит неподвижно.`);
+//   }
+// }
+class Rabbit extends Animal {
+  // генерируется для классов-потомков, у которых нет своего конструктора
+  constructor(name) {
+    super(name);
+  }
+
+  hide() {
+    console.log(`${this.name} прячется!`);
+  }
+
+  stop() {
+    super.stop();
+    this.hide();
+  }
+}
+
+// let rabbit = new Rabbit("Белый кролик");
+// console.log(rabbit);
+
+// super + hide
+
+// static
+// class User {
+//   constructor(name = "user") {
+//     this.name = name;
+//   }
+
+//   static staticMethod() {
+//     console.log(2 + 2);
+//   }
+// }
+
+// // const user = new User("John");
+// User.staticMethod();
